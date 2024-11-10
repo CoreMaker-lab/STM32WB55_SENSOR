737fd8e68642b367058a2b532810eb497c477cf8
CoreMaker <a845656974@outlook.com>
2024-11-10 15:45:42 +0800

    update

---
+++ STM32WB55_project3/MDK-ARM/STM32WB55_project3/ble_events.d
@@ -0,0 +1,11 @@
+stm32wb55_project3/ble_events.o: \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_events.c \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_events.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\ble_const.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\ble_std.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\ble_defs.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\osal.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\compiler.h
---
+++ STM32WB55_project3/MDK-ARM/STM32WB55_project3/osal.d
@@ -0,0 +1,4 @@
+stm32wb55_project3/osal.o: \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\osal.c \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\osal.h
