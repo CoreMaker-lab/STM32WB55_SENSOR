22bb7a0a191ea6aec8b89df43bf75b3b49f7a645
CoreMaker <a845656974@outlook.com>
2024-11-12 01:27:09 +0800

    update

---
+++ STM32WB55_project5/MDK-ARM/STM32WB55_project5/ble_hal_aci.d
@@ -0,0 +1,11 @@
+stm32wb55_project5/ble_hal_aci.o: \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_hal_aci.c \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_hal_aci.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\auto\ble_types.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\ble_const.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\ble_std.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\ble_defs.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\osal.h \
+  ..\Middlewares\ST\STM32_WPAN\ble\core\template\compiler.h
---
+++ STM32WB55_project5/MDK-ARM/STM32WB55_project5/hci_tl_if.d
@@ -0,0 +1,14 @@
+stm32wb55_project5/hci_tl_if.o: \
+  ..\Middlewares\ST\STM32_WPAN\interface\patterns\ble_thread\tl\hci_tl_if.c \
+  ..\Middlewares\ST\STM32_WPAN\interface\patterns\ble_thread\tl\hci_tl.h \
+  ..\Middlewares\ST\STM32_WPAN\stm32_wpan_common.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdlib.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdarg.h \
+  ..\Drivers\CMSIS\Include\cmsis_compiler.h \
+  ..\Drivers\CMSIS\Include\cmsis_armclang.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  ..\Middlewares\ST\STM32_WPAN\interface\patterns\ble_thread\tl\tl.h
